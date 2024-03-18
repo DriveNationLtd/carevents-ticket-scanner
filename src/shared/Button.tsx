6bd2bfeb38a02213dad794e00c877c90f1cb5e10
Keshanth Jude <keshanth64@gmail.com>
2024-03-18 13:51:23 +0000

    Fixed prop error

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -7,32 +7,31 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     fullPageLoading?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = (props) => {
+export const Button: React.FC<ButtonProps> = ({ fullPageLoading, icon, ...btnprops }) => {
     const { pending } = useFormStatus();
-    const isFullPageLoading = props.fullPageLoading;
 
     const renderIcon = () => {
-        if (pending && !isFullPageLoading) {
+        if (pending && !fullPageLoading) {
             return <i className="ml-2 fas fa-spinner fa-spin"></i>
         }
 
-        if (props.icon) {
+        if (icon) {
             return <i className="ml-2 fas fa-chevron-right"></i>
         }
     }
     return (
         <>
-            {isFullPageLoading && pending && <Loader />}
+            {fullPageLoading && pending && <Loader />}
             <button
-                {...props}
+                {...btnprops}
                 className={clsx(
                     "uppercase bg-theme-primary w-full hover:bg-theme-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline",
                     pending && "cursor-not-allowed bg-theme-primary-light",
-                    props.className
+                    btnprops.className
                 )}
                 disabled={pending}
             >
-                {props.children} {renderIcon()}
+                {btnprops.children} {renderIcon()}
             </button>
         </>
     );
