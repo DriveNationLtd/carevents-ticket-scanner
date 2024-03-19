463c26327624099264878b697431327ba1ebb3b2
Keshanth Jude <keshanth64@gmail.com>
2024-03-19 12:50:41 +0000

    Updated allowed server actions

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
     images: {
         domains: ['wordpress-889362-4267074.cloudwaysapps.com', 'www.carevents.com'],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["phpstack-889362-4370795.cloudwaysapps.com" /* or Codespace port forward url, no including scheme */, "localhost:3000"]
+        }
+    }
 }
 
 const withPWA = withPWAInit({
